0a07865e0a6dae8f84dc8486cacc7a95e7487293
runpixelrun <justindavidson23@gmail.com>
2015-08-20 12:31:17 +0000

    Modified sign-up form for paid plan

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
     def home
         @admin_plan = Plan.find(1)
         @partner_plan = Plan.find(2)
+        @paidsubscription_plan = Plan.find(3)
     end
 end
